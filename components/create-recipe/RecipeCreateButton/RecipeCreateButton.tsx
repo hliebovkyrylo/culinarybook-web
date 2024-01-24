08580bfe7e9434fde29ab947165275d3b605205c
Kyrylo Hliebov <kirilglebow030@gmail.com>
2024-01-24 17:30:23 +0200

    minor changes to create-recipe page layout

---
+++ components/create-recipe/RecipeCreateButton/RecipeCreateButton.tsx
@@ -0,0 +1,7 @@
+const RecipeCreateButton = () => {
+  return (
+    <button className="bg-bg-c-10 w-[234px] h-11 text-black rounded-md font-semibold mt-12">Create recipe</button>
+  )
+};
+
+export default RecipeCreateButton;
