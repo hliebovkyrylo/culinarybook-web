5c4b738d4bde4782ad24628532252c8e3eb631dd
Kyrylo Hliebov <kirilglebow030@gmail.com>
2024-05-02 09:55:56 +0300

    feat: connected logic to the notifications page

--- components/notifications/NotificationCommentReplyMessage/NotificationCommentReplyMessage.tsx
+++ components/notifications/NotificationCommentReplyMessage/NotificationCommentReplyMessage.tsx
@@ -1,12 +1,11 @@
-"use client"
-
 import useFormatDaysAgo   from "@/hooks/useFormatDaysAgo";
-import Image              from "next/image";
+import Image from "next/image";
 import Link               from "next/link";
 import { useTranslation } from "react-i18next";
 
-interface INotificationLikeMessage {
+interface INotificationCommentReplyMessage {
   userImage  : string;
+  commentText: string;
   username   : string;
   recipeImage: string;
   recipeId   : string;
@@ -14,40 +13,43 @@ interface INotificationLikeMessage {
   createdAt  : Date;
 }
 
-export const NotificationLikeMessage = ({
+export const NotificationCommentReplyMessage = ({
   userImage,
+  commentText,
   username,
   recipeImage,
   recipeId,
   userId,
   createdAt,
-}: INotificationLikeMessage) => {
+}: INotificationCommentReplyMessage) => {
   const { t } = useTranslation();
-
+  
   const formatDaysAgo = useFormatDaysAgo();
-
   return (
     <article className="w-full min-h-[64px] second-background flex items-center py-2 px-3 rounded-lg mb-2">
       <div className="flex justify-between w-full">
         <div className="flex justify-between">
-          <Link className="mr-3" href={`/profile/${userId}`}>
+          <Link className="mr-3 w-12 h-12" href={`/profile/${userId}`}>
             <Image src={userImage !== '' ? userImage : '/assets/defaulUserImage.jpg'} className="object-cover rounded-full w-12 h-12" alt="User image" width={48} height={48} />
           </Link>
           <div>
-            <div>
+            <div className="max-[516px]:!w-28">
               <Link className="text-sm" href={`/profile/${userId}`}><b>{username}</b></Link>
-              <span className="text-[#959595] text-sm"> {t('comment-type-like')}</span>
+              <span className="text-[#959595] text-sm"> {t('notification-type-reply')}</span>
             </div>
+            <p className="max-w-xs break-words text-sm overflow-hidden max-h-48 line-clamp-6">
+              {commentText}
+            </p>
             <div className="flex items-center">
               <span className="block w-1 h-1 bg-[#959595] rounded-full mr-1" />
               <p className="text-[#959595] text-sm">{formatDaysAgo(createdAt)}</p>
             </div>
           </div>
         </div>
-        <Link href={`/recipe/${recipeId}`}>
+        <Link className="w-12 h-12" href={`/recipe/${recipeId}`}>
           <Image src={recipeImage !== '' ? recipeImage : '/assets/no-image.png'} className="object-cover rounded-md w-12 h-12" alt="Recipe image" width={48} height={48} />
         </Link>
       </div>
     </article>
   )
-}
+};
--- components/notifications/NotificationFollowMessage/NotificationFollowMessage.tsx
+++ components/notifications/NotificationFollowMessage/NotificationFollowMessage.tsx
@@ -1,11 +1,12 @@
 "use client"
 
 import useFormatDaysAgo   from "@/hooks/useFormatDaysAgo";
+import Image from "next/image";
 import Link               from "next/link";
 import { useTranslation } from "react-i18next";
 
 interface INotificationFollowMessage {
-  userImage  : React.ReactNode | "";
+  userImage  : string;
   username   : string;
   userId     : string;
   createdAt  : Date;
@@ -25,11 +26,13 @@ export const NotificationFollowMessage = ({
     <article className="w-full min-h-[64px] second-background flex items-center py-2 px-3 rounded-lg mb-2">
       <div className="flex justify-between w-full">
         <div className="flex justify-between">
-          <Link href={`/profile/${userId}`}>{userImage}</Link>
+          <Link className="mr-3" href={`/profile/${userId}`}>
+            <Image src={userImage !== '' ? userImage : '/assets/defaulUserImage.jpg'} className="object-cover rounded-full w-12 h-12" alt="User image" width={48} height={48} />
+          </Link>
           <div>
             <div>
-              <Link href={`/profile/${userId}`}><b>{username}</b></Link>
-              <span> {t('comment-type-following')}</span>
+              <Link className="text-sm" href={`/profile/${userId}`}><b>{username}</b></Link>
+              <span className="text-sm"> {t('comment-type-following')}</span>
             </div>
             <div className="flex items-center">
               <span className="block w-1 h-1 bg-[#959595] rounded-full mr-1" />
