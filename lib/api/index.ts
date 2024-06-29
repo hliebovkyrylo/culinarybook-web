9f6ddc17f46034a2ad9d2091740accf505edff16
Kyrylo Hliebov <kirilglebow030@gmail.com>
2024-06-29 13:03:50 +0300

    feat: made automatic renewal of tokens

--- lib/api/index.ts
+++ lib/api/index.ts
@@ -1,23 +1,62 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import Cookies                       from "js-cookie";
+import { ApiError } from '@/typings/error';
+import { BaseQueryFn, FetchArgs, FetchBaseQueryError, createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import Cookies from "js-cookie";
+import { IAuthResponse } from './authApi';
 
 export const baseUrl = process.env.NEXT_PUBLIC_APP_API_URL as string;
 
-export const api = createApi({
-  reducerPath: 'api',
-  tagTypes   : ['user', 'recipe'],
-  baseQuery  : fetchBaseQuery({
-    baseUrl: baseUrl,
-    credentials: 'include',
-    prepareHeaders: (headers) => {
-      const accessToken = Cookies.get('access_token');
+const baseQuery = fetchBaseQuery({
+  baseUrl: baseUrl,
+  credentials: 'include',
+  prepareHeaders: (headers) => {
+    const accessToken = Cookies.get('access_token');
 
-      if (accessToken) {
-        headers.set('authorization', accessToken);
+    if (accessToken) {
+      headers.set('Authorization', accessToken);
+    }
+
+    return headers;
+  }
+});
+
+const baseQueryWithReauth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result.error && result.error.data && (result.error.data as ApiError).code === 'jwt-expired') {
+    const refreshResult = await baseQuery({ url: '/auth/refresh-token', method: 'POST' }, api, extraOptions);
+
+    if (refreshResult.data) {
+      const newAccessToken = (refreshResult.data as IAuthResponse).access_token;
+
+      Cookies.set('access_token', (refreshResult.data as IAuthResponse).access_token);
+
+      if (typeof args === 'string') {
+        args = { url: args }
+      }
+      if (!args.headers) {
+        args.headers = {}
+      }
+      
+      if (args.headers instanceof Headers) {
+        args.headers.set('Authorization', newAccessToken)
+      } else if (Array.isArray(args.headers)) {
+        args.headers.push(['Authorization', newAccessToken])
+      } else {
+        args.headers['Authorization'] = newAccessToken
       }
 
-      return headers;
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      Cookies.remove('access_token');
     }
-  }),
+  }
+
+  return result;
+}
+
+export const api = createApi({
+  reducerPath: 'api',
+  tagTypes: ['user', 'recipe'],
+  baseQuery: baseQueryWithReauth,
   endpoints: () => ({}),
 });
