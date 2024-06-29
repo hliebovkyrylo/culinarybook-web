0f8977491e9e93ff745ba68ee394591f8de4e056
Kyrylo Hliebov <kirilglebow030@gmail.com>
2024-06-29 14:51:03 +0300

    fix: loader at requireAuth hoc

--- hocs/requireAuth.tsx
+++ hocs/requireAuth.tsx
@@ -1,13 +1,16 @@
 import { useRouter } from "next/router";
 import { FunctionComponent } from "react";
 import { useGetMeQuery } from "@/lib/api/userApi";
+import { Loader } from "@/components/Loader";
 
 export const RequireAuth = <Props extends object>(Component: FunctionComponent<Props>) => (props: Props) => {
   const { data: user, isLoading } = useGetMeQuery();
 
   const router = useRouter();
 
-  if (isLoading) return null;
+  if (isLoading) {
+    return <Loader className="absolute top-0 left-0" />
+  };
 
   if (user?.isVerified === false) {
     router.push('/verify-account');
