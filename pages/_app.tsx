5558692cabda5a687c3367be8d05c7297ea6c760
Kyrylo Hliebov <kirilglebow030@gmail.com>
2024-07-05 13:41:18 +0300

    ci: connected google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,17 +5,30 @@ import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import "@/styles/globals.css";
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { GoogleTagManager } from '@next/third-parties/google'
+import Script from 'next/script'
 
 const MyApp: React.FunctionComponent<AppProps> = ({ Component, ...rest }) => {
   const { store, props } = wrapper.useWrappedStore(rest);
-  const gtmId = process.env.NEXT_PUBLIC_GTM_ID as string;
 
   return (
     <Provider store={store}>
       <ThemeProvider attribute='class'>
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+        />
+
+        <Script id="google-analytics-script" strategy="lazyOnload">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GTM_ID}', {
+            page_path: window.location.pathname,
+            });
+          `}
+        </Script>
         <Component {...props.pageProps} />
-        <GoogleTagManager gtmId={gtmId} />
       </ThemeProvider>
     </Provider>
   );
