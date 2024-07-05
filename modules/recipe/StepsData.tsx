297a3675e9f9fd1ab19e86af4e46e32f4d7ded1f
Kyrylo Hliebov <kirilglebow030@gmail.com>
2024-07-05 21:23:15 +0300

    fix: fixed text formatting in recipe steps

--- modules/recipe/StepsData.tsx
+++ modules/recipe/StepsData.tsx
@@ -31,7 +31,7 @@ export const StepsData = ({
               <SwiperSlide style={{ width: '300px' }} key={index}>
                 <div className="relative bg-[#cccccc4b] dark:bg-[#41414159] break-words p-3 rounded-xl min-h-[128px]">
                   <span className="left-3 top-2 link-text font-semibold z-50">{t('step')} {index + 1}</span>
-                  <pre className="font-montserrat text-base">{step.stepDescription}</pre>
+                  <div className="font-montserrat text-base">{step.stepDescription}</div>
                 </div>
               </SwiperSlide>
             ))}
