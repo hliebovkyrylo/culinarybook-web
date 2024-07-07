6b036d180191bab3a20294285f6b2159387f0e03
Kyrylo Hliebov <kirilglebow030@gmail.com>
2024-07-08 01:11:15 +0300

    ci: completed robots.txt and sitemap.xml generations

---
+++ next-sitemap.config.js
@@ -0,0 +1,41 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL,
+  generateRobotsTxt: true,
+  alternateRefs: [
+    {
+      href: `${process.env.SITE_URL}/ru`,
+      hreflang: 'ru',
+    },
+    {
+      href: `${process.env.SITE_URL}/uk`,
+      hreflang: 'uk',
+    },
+  ],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/confirm-code',
+          '/forgot-password',
+          '/change-password',
+          '/reset-password',
+          '/ru/confirm-code',
+          '/ru/forgot-password',
+          '/ru/change-password',
+          '/ru/reset-password',
+          '/uk/confirm-code',
+          '/uk/forgot-password',
+          '/uk/change-password',
+          '/uk/reset-password',
+        ],
+      },
+    ],
+    additionalSitemaps: [
+      `${process.env.SITE_URL}/sitemap.xml`,
+      `${process.env.SITE_URL}/sitemap-0.xml`,
+    ],
+  },
+}
