72e33be1fe38d89f45c6ca9ed1620aba63504349
hliebovkyrylo <kirilglebov06@gmail.com>
2024-11-19 22:36:56 +0200

    fix: changed loader color

--- components/Loader/Loader.tsx
+++ components/Loader/Loader.tsx
@@ -3,7 +3,7 @@ export const Loader = ({ className }: { className?: string }) => {
     <div
       className={`w-full h-full flex justify-center items-center z-50 dark:bg-bg-c bg-bg-c-7 ${className}`}
     >
-      <div className="dark:border-[#222222] border-neutral-300 h-14 w-14 animate-spin rounded-full border-8 dark:border-t-[#DDDF72] border-t-[#C77D0A]" />
+      <div className="dark:border-[#222222] border-neutral-300 h-14 w-14 animate-spin rounded-full border-8 border-t-[#DB8961]" />
     </div>
   );
 };
